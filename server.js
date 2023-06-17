e5e9632a145757e675024f2b302feb643a091a5e
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-06-17 22:45:44 +0900

    feat: run on express graphql server

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var { graphql, buildSchema } = require("graphql");
+var express = require("express");
+var { graphqlHTTP } = require("express-graphql");
+var { buildSchema } = require("graphql");
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
@@ -7,18 +9,21 @@ var schema = buildSchema(`
   }
 `);
 
-// The rootValue provides a resolver function for each API endpoint
-var rootValue = {
+// The root provides a resolver function for each API endpoint
+var root = {
   hello: () => {
     return "Hello world!";
   },
 };
 
-// Run the GraphQL query '{ hello }' and print out the response
-graphql({
-  schema,
-  source: "{ hello }",
-  rootValue,
-}).then((response) => {
-  console.log(response);
-});
+var app = express();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphql: true,
+  })
+);
+app.listen(4000);
+console.log("Running a GraphQL API server at http://localhost:3000/graphql");
